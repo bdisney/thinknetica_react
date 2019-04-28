a367d41fed57b23a0c17cb62b0388c0484608cfb
disne_y <bold.disney@gmail.com>
2019-04-28 17:24:22 +0300

    render Calculate results via react

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log('Hello, world!');
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+import Calculate from '~/src/modules/Calculate';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <div>{Calculate.sum(2, 2)}</div>
+        <div>{Calculate.subtract(42, 2)}</div>
+        <div>{Calculate.multiply(3, 3)}</div>
+        <div>{Calculate.divide(4, 4)}</div>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
